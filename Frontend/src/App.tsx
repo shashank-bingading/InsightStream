0272ce7254ceed0b52ca59505a901c3ad4b10ca7
Shashank Singh <shashanksinghwork88@gmail.com>
2026-07-20 18:51:41 +0530

    Initialise theme and basic UI elements

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css'
+import {useState} from 'react'
+import Sidebar from './components/Sidebar'
 
 function App() {
-  
+  const [inputText,setInputText] = useState("");
+  const [sentiment,setSentiment] = useState(true);
+  const [summarise,setSummarise] = useState(true);
+
 
   return (
-    <>
-    </>
+    <div className='min-h-screen bg-main-bg text-text-primary flex'>
+    <Sidebar/>
+    <main className='flex-1 p-10 max-w-5xl mx-auto'>
+
+    </main>
+    </div>
   )
 }
 
---
+++ Frontend/src/components/Sidebar.tsx
@@ -0,0 +1,19 @@
+
+const Sidebar = ({}) => {
+  return (
+    <aside className='p-6 w-64 bg-surface border-r border-text-secondary/15 h-screen'>
+      <h2 className=' text-accent font-bold text-xl tracking-wider'>
+        INSIGHT STREAM
+      </h2>
+      <nav className='space-y-3 mt-8'>
+        <a href="
+        " className='block py-2 px-4 bg-main-bg  text-text-secondary hover:text-text-primary'>DASHBOARD</a>
+
+        <a href="
+        " className=" block px-4 py-2 text-text-secondary bg-main-bg hover:text-text-primary">HISTORY</a>
+      </nav>
+    </aside>
+  )
+}
+
+export default Sidebar
